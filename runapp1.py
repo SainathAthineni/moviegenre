60fad1de1a17ab8eb63157e85397ce7c2ce57e78
Sainath Athineni <150629074+SainathAthineni@users.noreply.github.com>
2024-07-16 01:39:58 -0400

    Add files via upload

---
+++ runapp1.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import joblib
+import numpy as np
+# Load the model and label binarizer
+model = joblib.load('C:/Users/saina/Downloads/genre_model.pkl')
+mlb = joblib.load('C:/Users/saina/Downloads/mlb.pkl')
+# Streamlit interface
+st.title("Movie Genre Prediction App")
+# Input movie description
+description = st.text_area("Enter movie description:")
+
+if st.button("Predict Genre"):
+    if description:
+        # Predict the genre
+        pred = model.predict([description])
+        pred_genres = mlb.inverse_transform(pred)
+        
+        # Display the results
+        st.write("Predicted Genres:")
+        for genre in pred_genres[0]:
+            st.write(f"- {genre}")
+    else:
+        st.write("Please enter a movie description.")
